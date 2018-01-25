cafbdffd59364325b08d4d0e3d377b6ffb64d208
Sunil Kumar <sunil.brahmajosyula@rubiconred.com>
2018-01-25 15:57:49 +0530

    added all the modules

---
+++ learn/parent/backend/core/src/main/java/org/s/learn/core/App.java
@@ -0,0 +1,13 @@
+package org.s.learn.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
